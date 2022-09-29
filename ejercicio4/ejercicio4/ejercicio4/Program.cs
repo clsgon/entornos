6c98e9cacf7c13b997b78406c502ec01ce197997
clsgon <hercarlos@hotmail.es>
2022-09-29 10:33:29 +0200

    debug version 2

--- ejercicio4/ejercicio4/ejercicio4/Program.cs
+++ ejercicio4/ejercicio4/ejercicio4/Program.cs
@@ -1,21 +1,23 @@
 ﻿Console.WriteLine("Demo de calculadora");
 
 Console.Write("Empecemos con una suma: ");
-const int SUMA = 2 + 2 + 6 + 3 + 2;
-Console.WriteLine("2 + 2 + 6 + 3 + 2 = " + SUMA);
+const int SUMA = 3 + 2 + 6 + 3 + 12;
+Console.WriteLine("3 + 2 + 6 + 3 + 12 = " + SUMA);
 
 Console.Write("Sigamos con una resta: ");
-const int RESTA = 2 - 2 - 6 - 3 - 2;
-Console.WriteLine("2 - 2 - 6 - 3 - 2 = " +  RESTA);
+const int RESTA = 2 - 12 - 6 - 13 - 2;
+Console.WriteLine("2 - 12 - 6 - 13 - 2 = " +  RESTA);
 
 Console.Write("Continuemos con una multiplicación: ");
-const int MULT = 2 * 2 * 6 * 3 * 2;
-Console.WriteLine("2 * 2 * 6 * 3 * 2 = " +  MULT);
+const int MULT = 2 * 2 * 6 * 3 * 12;
+Console.WriteLine("2 * 2 * 6 * 3 * 12 = " +  MULT);
 
 Console.Write("A continuación una división: ");
-const float DIV = 12 / 4;
-Console.WriteLine("12 / 4 = " +  DIV);
+const float DIV = 12 / 3;
+Console.WriteLine("12 / 3 = " +  DIV);
 
 Console.Write("Por último, una exponencial: ");
-double exp = Math.Pow(12, 2);
-Console.WriteLine("12 elevado a 2 es " + exp);
+double exp = Math.Pow(12, 7);
+Console.WriteLine("12 elevado a 7 es " + exp);
+
+Console.WriteLine();
