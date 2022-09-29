658b2ce7a5d6d3621e14840623208171b4ce579b
clsgon <hercarlos@hotmail.es>
2022-09-29 10:00:48 +0200

    ejercicio 4 version 1

---
+++ ejercicio4/ejercicio4/ejercicio4/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.WriteLine("Demo de calculadora");
+
+Console.Write("Empecemos con una suma: ");
+const int SUMA = 2 + 2 + 6 + 3 + 2;
+Console.WriteLine("2 + 2 + 6 + 3 + 2 = " + SUMA);
+
+Console.Write("Sigamos con una resta: ");
+const int RESTA = 2 - 2 - 6 - 3 - 2;
+Console.WriteLine("2 - 2 - 6 - 3 - 2 = " +  RESTA);
+
+Console.Write("Continuemos con una multiplicación: ");
+const int MULT = 2 * 2 * 6 * 3 * 2;
+Console.WriteLine("2 * 2 * 6 * 3 * 2 = " +  MULT);
+
+Console.Write("A continuación una división: ");
+const float DIV = 12 / 4;
+Console.WriteLine("12 / 4 = " +  DIV);
+
+Console.Write("Por último, una exponencial: ");
+double exp = Math.Pow(12, 2);
+Console.WriteLine("12 elevado a 2 es " + exp);
