a40de9ddc8eb43a2f8579989c64faf2b4349a586
clsgon <hercarlos@hotmail.es>
2022-10-20 16:41:10 +0200

    compilacion con cmd de csharp con windows

---
+++ csharp/comp1/windows/prueba.cs
@@ -0,0 +1,17 @@
+using System;
+
+namespace HelloWorld{
+
+	class Program{
+		
+		static void Main(string[] args){
+			
+			Console.Write("Introduce su nombre: ");
+			string name = Console.ReadLine();
+			Console.WriteLine("Hola, " + name);
+				
+		}		
+
+	}
+
+}
