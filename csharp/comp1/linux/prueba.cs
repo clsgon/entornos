1cf2aa89c13583dd0e102d39fbafc379faf21164
Carlos <hercarlos@hotmail.es>
2022-10-20 18:26:42 +0200

    compilacion csharp terminal

---
+++ csharp/comp1/linux/prueba.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace HolaMundo{
+
+   class Program{
+   
+      static void Main(){
+        
+        Console.Write("¿Cuál es tu nombre?: ");
+        string nombre = Console.ReadLine();
+        Console.WriteLine("Hola, " + nombre);
+        
+      }
+   }
+}
