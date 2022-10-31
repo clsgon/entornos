fc524b087efe27401d99b30786b9be14cc25bcf6
Carlos <hercarlos@hotmail.es>
2022-10-31 15:40:13 +0100

    Ejercicio de compilación de C desde la línea de comandos

---
+++ c/ejercicio1/name.c
@@ -0,0 +1,11 @@
+int main(void){
+
+  char name[10];
+  int int1, int2;
+  float decimal;
+  
+  printf("Introduce tu nombre: ");
+  scanf("%s", name);
+  printf("Te llamas %s \n", name);
+  return 0;
+}
