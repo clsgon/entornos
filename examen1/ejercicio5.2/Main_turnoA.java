bd1bc3367ba06dc8545b0c838e75e73db941b89a
clsgon <hercarlos@hotmail.es>
2022-11-03 09:30:42 +0100

    examen

---
+++ examen1/ejercicio5.2/Main_turnoA.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.main_turnoa;
+
+/**
+ *
+ * @author herca
+ */
+public class Main_turnoA {
+
+    public static void main(String[] args) {
+        int c=9;
+        int a=8;
+        double d=Math.cosh(a/c)*Math.cos(a)+10*Math.random();
+        System.out.println("Fin del programa turno A");
+    }
+}
