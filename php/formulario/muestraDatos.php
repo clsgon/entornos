aeb44e2c44d466dd045e2faf3452e9a53c48a4c0
Carlos <hercarlos@hotmail.es>
2022-11-24 10:05:05 +0100

    formulario carlos gonzález

---
+++ php/formulario/muestraDatos.php
@@ -0,0 +1,13 @@
+<?php
+$name = $_POST["name"];
+$surname = $_POST["surname"];
+
+echo "Hola $name $surname";
+
+?>
+
+<html>
+    <head>
+        <title>Carlos González</title>
+    </head>
+</html>
